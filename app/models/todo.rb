dbb643201fb3cd1d78cec1d41d981caf49ff4409
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-03-09 20:48:08 +0530

    Display list of todos with formatted date

---
+++ app/models/todo.rb
@@ -0,0 +1,9 @@
+class Todo < ActiveRecord::Base
+    def to_pleasant_string
+        is_completed = completed ? "[X]": "[ ]"
+        "#{id}. #{formatted_due_date} #{todo_text} #{is_completed}"
+    end
+    def formatted_due_date
+        due_date == nil ? "no due date set,":due_date.to_s(:long)     
+    end
+end
