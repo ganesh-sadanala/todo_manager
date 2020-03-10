98419d85f6b47119b474586581aa0cfefd720e96
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-03-10 18:37:29 +0530

    Use the resources routing convention

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     render plain: Todo.order(:due_date).map{|todo| todo.to_pleasant_string}.join("\n")
   end
@@ -17,6 +18,5 @@ class TodosController < ApplicationController
     )
     response_text="Your new todo is created with id: #{new_todo.id}"
     render plain: response_text
-
   end
 end
