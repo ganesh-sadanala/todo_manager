7c6fda05e14b1853a6207a699a55fc2d13700bed
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-04-16 09:45:49 +0530

    Add a new todo with the help of form tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_121448) do
+ActiveRecord::Schema.define(version: 2020_04_12_070129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,4 +26,5 @@ ActiveRecord::Schema.define(version: 2020_03_11_121448) do
     t.string "email"
     t.string "password"
   end
+
 end
