5ea65578a6ce4d1e1c12c8dfa2b982a077af9d21
ganesh-sadanala <ganesh8374387106@gmail.com>
2020-04-27 12:29:24 +0530

    Add signing-in, updating session, fencing-off protected routers

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,7 @@
 class TodosController < ApplicationController
   def index
     # render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    @current_user = current_user
     render "index"
   end
 
